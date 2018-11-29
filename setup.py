b07e710f46ca7936625218612de87f04d0190849
James E.H. Turner <jturner@gemini.edu>
2018-11-29 17:18:47 -0300

    Add a minimal setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyspex',
+      version='0.1',
+      description='Python spectroscopy experiment',
+      url='https://github.com/jehturner/pyspex',
+      author='James Turner',
+      license='BSD',
+      packages=['pyspex'],
+      zip_safe=False)
+
